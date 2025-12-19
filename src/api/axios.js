b13f762a41dc2b45749b180c9e9cb4ba14621bfd
Ankit Kumar Yadav <ankityadav18193@gmail.com>
2025-12-19 18:59:56 +0530

    Updated axios base URL and fixed auth api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://gollo-backend.onrender.com",
+  baseURL: "https://gollo-backend.onrender.com/api",
   withCredentials: false,
 });
 
